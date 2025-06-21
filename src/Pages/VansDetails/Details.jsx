9ef07147a7fe32aa282809c63cd80e4ea7ec52eb
CandO01 <119475643+CandO01@users.noreply.github.com>
2025-06-21 12:57:31 +0100

    Update Details.jsx
    from local host to remote host

--- src/Pages/VansDetails/Details.jsx
+++ src/Pages/VansDetails/Details.jsx
@@ -8,7 +8,7 @@ function Details() {
 
   useEffect(()=>{
     async function vanInfo() {
-      const res = await fetch(`http://localhost:8254/api/host/vans/${params.id}`)
+      const res = await fetch(`https://vanlife-api-8k5o.onrender.com/api/host/vans/${params.id}`)
       const data = await res.json()
       setInfo(data)
     }
