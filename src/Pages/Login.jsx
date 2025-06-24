48a189d88f81373f30d14e143fe681cad9e10e0b
CandO01 <119475643+CandO01@users.noreply.github.com>
2025-06-24 01:56:04 +0100

    Update Login.jsx
    updated features in the login component

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useState } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useNavigate } from 'react-router-dom'
 function Login() {
  const [loginFormData, setLoginFormData] = useState({email: "", password: ""})
-
+ const [status, setStatus] = useState('idle')
+ const [error, setError] = useState(null)
  const location = useLocation()
+ const navigate = useNavigate()
 
  //function to handle the submit button
 
  async function handleSubmit(){
+   setStatus('submitting')
+   setError(null)
     try {
      
      const url = 'https://vanlife-api-8k5o.onrender.com/login'
@@ -24,13 +28,15 @@ function Login() {
 
     if (res.ok) {
       console.log('✅ Success:', data);
-      window.location.href = data.redirect; // Or use React Router's navigate('/host')
+      navigate('/host')
     } else {
       console.error('❌ Error:', data.error);
       alert(data.error);
     }
   } catch (err) {
-    console.error('⛔ Network Error', err);
+    setError(err);
+  }finally{
+     setStatus('idle')
   }
  }
 
@@ -54,6 +60,13 @@ function Login() {
         userMessage && <h3 style={{color: 'red'}}>{location.state.message}</h3>
       }
       <h1>Sign into your account</h1>
+     {
+       error && (
+        <p style={{color:'red', fontWeight: 800}}>
+         {error.message}
+        </p>
+         )
+     }
       <form action={handleSubmit} className='login-form'>
         <input 
           type="email"
@@ -69,7 +82,7 @@ function Login() {
           onChange={handleChange}
           value={loginFormData.password}
         />
-        <button type="submit">Login</button>
+        <button type="submit" disabled={status==='submitting'}>{status==='submitting' ? 'Loggin in...' : 'Login'}</button>
       </form>
       <p>Don't have an account? <span style={{color:'#FF8C38', fontWeight:'bold', cursor: 'pointer' }}>Create one now</span></p>
     </div>
